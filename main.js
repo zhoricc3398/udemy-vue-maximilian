d746ea467d20bf88d1d5e4dc0fe60dd1bc926cec
zhoricc3398 <16200126@ibsu.edu.ge>
2019-08-27 18:28:20 +0400

    feat(source): finish course 24

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 new Vue({
   el: "#app",
   data: {
-    name: 'Max'
+    counter: 0,
+    secondCounter: 0
+  },
+  computed: {
+    output: function() {
+      console.log("Computed!");
+      return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
+    }
+  },
+  methods: {
+    result: function() {
+      console.log("Method!");
+      return this.counter > 5 ? "Greater than 5" : "Smaller than 5";
+    }
   }
 });
