2825c20244c0e818bab5e034726fdac0faf29472
zhoricc3398 <16200126@ibsu.edu.ge>
2019-08-29 10:46:57 +0400

    feat(source): finish course 35

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 new Vue({
   el: "#app",
   data: {
-    color: "gray",
-    width: 100
-  },
-  computed: {
-    myStyle: function() {
-      return {
-        backgroundColor: this.color,
-        width: this.width + "px"
-      };
-    }
+    show: true
   }
 });
